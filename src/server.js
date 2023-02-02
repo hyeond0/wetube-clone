2b88e458ea33507b8a37c962728648e12e960323
hyeond0 <a98792530@gmail.com>
2023-02-02 21:51:55 +0900

    Initial Settings

--- src/server.js
+++ src/server.js
@@ -8,31 +8,12 @@ const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
+
 const handleHome = (req, res) => {
   return res.send("I love middlewares.");
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-app.use(logger);
-app.use(privateMiddleware);
-
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
