d78e01058761640de03214547a9d783e23e346f5
hyeond0 <a98792530@gmail.com>
2023-02-02 21:51:04 +0900

    Study Middleware

--- src/server.js
+++ src/server.js
@@ -4,15 +4,36 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
+  return res.send("I love middlewares.");
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
 };
 
 const handleLogin = (req, res) => {
   return res.send("Login here.");
 };
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+app.use(logger);
+app.use(privateMiddleware);
+
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
