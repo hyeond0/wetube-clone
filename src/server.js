cb6aafe58aa37f6fffa35c82ba894b362fe7ebe8
hyeond0 <a98792530@gmail.com>
2023-02-05 18:01:53 +0900

    Study Router

--- src/server.js
+++ src/server.js
@@ -1,19 +1,39 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+const home = (req, res) => {
+  return res.send("Go to homepage.");
 };
 
-const handleHome = (req, res) => {
-  return res.send("I love middlewares.");
+const login = (req, res) => {
+  return res.send("Go to login page.");
 };
 
-app.get("/", logger, handleHome);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
