b1e7805d5d1d292715eeb595f152e21f22eebe00
hyeond0 <a98792530@gmail.com>
2023-02-09 16:10:02 +0900

    study post method

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
-  edit,
+  getEdit,
+  postEdit,
   watch,
   deleteVideo,
   upload,
@@ -8,8 +9,7 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id/(\\d+)edit", edit);
-videoRouter.get("/:id/(\\d+)delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
