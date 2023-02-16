60633f36e83b45441e5a2a742865b0b5fa1047eb
hyeond0 <a98792530@gmail.com>
2023-02-16 17:00:08 +0900

    mongodb 초기 세팅

--- src/db.js
+++ src/db.js
@@ -1,3 +1,10 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/");
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+db.on("error", (error) => console.log("DB Error", error));
+db.once("open", handleOpen);
