95cbbc333222398e78b42bf9978825ec76058da2
hyeond0 <a98792530@gmail.com>
2023-02-21 17:56:36 +0900

    Study POST Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/User";
-
+import bcrypt from "bcrypt";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
@@ -12,44 +12,44 @@ export const postJoin = async (req, res) => {
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.status(404).render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username/email is already taken.",
     });
   }
-  try {
-    await User.create({
-      name,
-      username,
-      email,
-      password,
-      location,
-    });
-    return res.redirect("/login");
-  } catch (error) {
-    return res.status(404).render("join", {
-      pageTitle,
-      errorMessage: "This username/email is already taken.",
-    });
-  }
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const getLogin = (req, res) => {
   return res.render("login", { pageTitle: "Log In" });
 };
+
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
-  if (!exists) {
+  const pageTitle = "Log In";
+  const user = await User.findOne({ username });
+  if (!user) {
     return res.status(404).render("login", {
-      pageTitle: "Log In",
+      pageTitle,
       errorMessage: "An account with this username does not exists.",
     });
   }
-  // 계정 일치 확인
-  // 패스워드 일치 확인
-  res.end();
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(404).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  res.redirect("/");
 };
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see");
