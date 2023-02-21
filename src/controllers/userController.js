46728d840837ff7691c993a608ae7aa5140738b0
hyeond0 <a98792530@gmail.com>
2023-02-21 16:53:47 +0900

    Create Login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,17 +17,39 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(404).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("login");
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Log In" });
+};
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res.status(404).render("login", {
+      pageTitle: "Log In",
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // 계정 일치 확인
+  // 패스워드 일치 확인
+  res.end();
+};
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see");
