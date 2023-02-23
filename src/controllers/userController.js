f010d8d9a12394423d8d9aa481fe6526509e3a42
hyeond0 <a98792530@gmail.com>
2023-02-23 20:50:05 +0900

    OAuth email

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,34 +56,61 @@ export const postLogin = async (req, res) => {
 };
 
 export const startGithubLogin = (req, res) => {
-  const baseUrl = "https://github.com/login/oauth/authorize";
+  const baseURL = "https://github.com/login/oauth/authorize";
   const config = {
     client_id: process.env.GITHUB_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
   const params = new URLSearchParams(config).toString();
-  const finalUrl = `${baseUrl}?${params}`;
-  return res.redirect(finalUrl);
+  const finalURL = `${baseURL}?${params}`;
+  return res.redirect(finalURL);
 };
 export const finishGithubLogin = async (req, res) => {
-  const baseUrl = "https://github.com/login/oauth/access_token";
+  const baseURL = "https://github.com/login/oauth/access_token";
   const config = {
     client_id: process.env.GITHUB_CLIENT,
     client_secret: process.env.GITHUB_SECRET,
     code: req.query.code,
   };
   const params = new URLSearchParams(config).toString();
-  const finalUrl = `${baseUrl}?${params}`;
-  const data = await fetch(finalUrl, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  });
-  const json = await data.json();
-  console.log(json);
-  res.send(JSON.stringify(json));
+  const finalURL = `${baseURL}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiURL = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiURL}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiURL}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const email = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!email) {
+      return res.redirect("/login");
+    }
+  } else {
+    return res.redirect("/login");
+    // 에러 창 추가 예정
+  }
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
