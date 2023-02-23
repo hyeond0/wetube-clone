4724e8b6b7b474134686dd28c0cd6d8ead727a04
hyeond0 <a98792530@gmail.com>
2023-02-23 17:12:24 +0900

    study .env, dotenv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
@@ -16,10 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    cookie: {
+      maxAge: 20000,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
