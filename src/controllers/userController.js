fa85490f94922b6d419d7d955e1251cd7fea8a83
hyeond0 <a98792530@gmail.com>
2023-02-26 15:19:41 +0900

    User 프로필에 업로드 비디오 출력

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import bcrypt from "bcrypt";
 import fetch from "node-fetch";
 import session from "express-session";
@@ -194,5 +195,10 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(400).render("404", { pageTitle: "User Not Found." });
   }
-  return res.render("user/profile", { pageTitle: `${user.name}`, user });
+  const videos = await Video.find({ owner: user._id });
+  return res.render("user/profile", {
+    pageTitle: `${user.name}`,
+    user,
+    videos,
+  });
 };
