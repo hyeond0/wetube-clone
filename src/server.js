e4a3eb412020de325c91c370e535b5f9174fdd49
hyeond0 <a98792530@gmail.com>
2023-03-06 10:06:08 +0900

    build backend

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   });
 });
 
-app.user(flash());
+app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
